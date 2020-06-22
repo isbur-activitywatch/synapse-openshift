25832033002116ae65cc64c7a1b1acc966409359
Igor Burenkov <43926915+isbur@users.noreply.github.com>
2020-06-23 02:27:02 +0300

    Update customized-run.sh

--- customized-run.sh
+++ customized-run.sh
@@ -1,8 +1,9 @@
 #!/bin/ash
 
 # Outer part of script is from here: https://unix.stackexchange.com/a/202276
-if    ls -1qA /data | grep -q .
+if    ls -1A /data | grep -q .
 then  ! echo data is not empty
 else  cp -r /tmp/src/data /data
 fi
+echo Before python script
 python /start.py
