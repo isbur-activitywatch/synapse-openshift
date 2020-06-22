a93200bb5fe028ba7e4e514131ac0a8e4791546a
Igor Burenkov <43926915+isbur@users.noreply.github.com>
2020-06-23 02:18:54 +0300

    Update customized-run.sh

--- customized-run.sh
+++ customized-run.sh
@@ -1,14 +1,8 @@
-#!/bin/sh
+#!/bin/ash
 
-print('Hello world!')
-
-# #!/bin/bash
-
-# echo We are inside!
-
-# # Outer part of script is from here: https://unix.stackexchange.com/a/202276
-# if    ls -1qA /data | grep -q .
-# then  ! echo data is not empty
-# else  cp -r /tmp/src/data /data
-# fi
-# python /start.py
+# Outer part of script is from here: https://unix.stackexchange.com/a/202276
+if    ls -1qA /data | grep -q .
+then  ! echo data is not empty
+else  cp -r /tmp/src/data /data
+fi
+python /start.py
